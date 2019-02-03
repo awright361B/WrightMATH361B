7badd450a6742f18d5c86a90bf259ba134fa93cb
aleciawMATH361B <aleciawright98@gmail.com>
2019-02-03 14:30:58 -0500

    Final Commit for I.2.

---
+++ IntrotoProgramming/Calculator_Wright.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  3 12:34:52 2019
+
+@author: aleci
+"""
+
+
+x=2
+y=6
+z=12
+mylist = [x+y, y*z+3*x, x**2, 11/2, 7 % 3]
+print ([x+y, y*z+3*x, x**2, 11/2, 7 % 3])
+z+3
+print(z+3)
+mylist.insert(3, 15)
+print(mylist)
+
+print(12*(3/4))
+
+mylist.append((12*(3/4)))
+print(mylist)
+
+print('The sum of the elements in the list is:', sum(mylist))
