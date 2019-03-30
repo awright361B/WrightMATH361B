4acae2d474e58e715d65d5995899e7f95231acbc
aleciawMATH361B <aleciawright98@gmail.com>
2019-03-29 23:11:32 -0400

    Final Commit for N6.

---
+++ NumberTheory/NumberTheory/N6_Amicable_Alecia.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 12:13:40 2019
+
+@author: aleci
+"""
+
+def d (x):
+    l = []
+    for i in range (1,x-1): 
+        if x/i == int(x/i): 
+            l.append(i)
+    print(l)
+
+
+N=1000
+x=284
+y=220
+def sum_divisor (x,y):
+    for i in range (1,N):
+        if sum_divisor == y and sum_divisor(y) == x:
+            return True
+    else:
+        return False
+print(sum_divisor(284,220))
+    
+        
+    
+    
